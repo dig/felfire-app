5705fa0bd0fbdc3b1bfbe1bd1db06e4d76a88e79
Joseph <joseph42@live.co.uk>
2019-11-09 02:48:41 +0000

    Continue login UI

--- app/src/main/ui/Login.js
+++ app/src/main/ui/Login.js
@@ -3,8 +3,14 @@ const { ipcRenderer } = require('electron');
 import React from 'react';
 
 class Login extends React.Component {
-  handleSubmit() {
+  constructor() {
+    super();
 
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
   }
 
   render() {
@@ -15,6 +21,18 @@ class Login extends React.Component {
             <h3>Login</h3>
             <small>Please enter your credentials to login.</small>
           </div>
+
+          <form onSubmit={this.handleSubmit}>
+            <div className="group">
+              <label>Email</label>
+              <input type="text" placeholder="Enter email" title="Enter your email" required name="email"></input>
+            </div>
+
+            <div className="group">
+              <label>Password</label>
+              <input type="text" placeholder="Enter password" title="Enter your password" required name="password"></input>
+            </div>
+          </form>
         </div>
       </div>
     )
