f7dc5498000172eb0f138cf93cb617fc688d64de
Joseph <joseph42@live.co.uk>
2019-11-12 23:09:38 +0000

    No errors from password reset

--- app/src/render/utils/User.js
+++ app/src/render/utils/User.js
@@ -30,7 +30,7 @@ exports.requestPasswordReset = (email) => {
   return new Promise((resolve, reject) => {
     request.post({url: `${apiURL}/users/forgot/password`, form: {email: email}, json: true}, function(error, response, body) {
       if (error || response.statusCode != 201) {
-        reject(body.errors);
+        reject();
       } else {
         resolve();
       }
